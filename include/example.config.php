2c917f0e9692720211427c4601135053258f85d1
luigi <luigifracalanza@gmail.com>
2018-10-22 14:19:22 -0300

    template of config.php

---
+++ include/example.config.php
@@ -0,0 +1,7 @@
+<?php
+//ML     CREDENCIAIS fornecidas pelo mercado livre
+$app_Id = "";
+$secret_Key = "";
+$user_id = "";
+$site_id = "";          // Enable TLS encryption, `ssl` also accepted
+?>
