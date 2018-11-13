060f342a9425381b62f111ee649ef1d58ac51f30
luigi <luigifracalanza@gmail.com>
2018-11-13 17:42:09 -0200

    adding product_image.php and orders.php

--- include/all_include.php
+++ include/all_include.php
@@ -1,7 +1,12 @@
 <?php
-require_once 'include/config.php';
-require_once 'include/funcoes.php';
 require_once 'include/ml/php-sdk/Meli/meli.php';
 require_once 'include/ml/php-sdk/configApp.php';
-require_once 'include/class_perguntas_respostas.php';
+
+require_once 'include/config.php';
+require_once 'include/defines.php';
+
+require_once 'include/meli.php';
+require_once 'include/products.php';
+require_once 'include/product_image.php';
+require_once 'include/orders.php';
 ?>
